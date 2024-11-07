0e295bcc91df184455eda11f120eed9566a517d2
HerokuSL <ddilisha78@gmail.com>
2024-11-07 15:37:07 +0530

    Update koyeb.js

--- koyeb.js
+++ koyeb.js
@@ -5,7 +5,7 @@ RUN sudo dnf -y update &&\
     sudo dnf install -y git ffmpeg ImageMagick nodejs yarnpkg libwebp &&\
     sudo dnf clean all -y
 
-RUN git clone https://github.com/GlobalTechInfo/ULTRA-MD
+RUN git clone https://github.com/GlobalTechInfo/SINGLE-MD
 
 WORKDIR /root/ultra-md
 
